05ecc7be4d77dd6235b166ad7e62ded759d9835b
AnneSon <slhyuny@gmail.com>
2021-09-09 18:02:08 -0700

    admin php

---
+++ admin/index.php
@@ -0,0 +1,17 @@
+<html>
+  <head>
+   <title>Galbi Restaurant - Home PAge</title>
+  </head>
+ 
+  <body>
+   <!-- Menu Section Starts -->
+   <!-- Menu Section Ends -->
+
+   <!-- Main Content Section Starts -->
+   <!-- Main Content Section Ends -->
+
+   <!-- Footer Section Starts -->
+   <!-- Footer Section Ends -->
+
+  </body>
+</html>
