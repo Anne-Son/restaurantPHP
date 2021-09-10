236ebe6b9759952e1b38bf054d6e15eeebffff5b
AnneSon <slhyuny@gmail.com>
2021-09-09 20:06:43 -0700

    admin style

--- admin/index.php
+++ admin/index.php
@@ -1,16 +1,44 @@
 <html>
   <head>
    <title>Galbi Restaurant - Home PAge</title>
+   <link rel="stylesheet" href="../css/admin.css">
+
   </head>
  
   <body>
    <!-- Menu Section Starts -->
+   <div class="menu text-center">
+     <div class="wrapper">
+     <ul>
+      <li><a href="#">HOME</a></li>
+      <li><a href="#">ADMIN</a></li>
+      <li><a href="#">CATEGORY</a></li>
+      <li><a href="#">FOOD</a></li>
+      <li><a href="#">ORDER</a></li>
+
+     </ul>
+     </div>
+    
+   </div>
    <!-- Menu Section Ends -->
 
    <!-- Main Content Section Starts -->
+   <div class="main-content">
+    <div class="wrapper">
+    Main goes here
+    </div>
+   </div>
    <!-- Main Content Section Ends -->
 
    <!-- Footer Section Starts -->
+   <div class="footer">
+    <div class="wrapper">
+        <p class="text-center">
+          &copy 2021. All rights reserved. Galbi Restaurant. Designed by
+          <a href="www.anneson.com"> Anne Son </a>
+        </p>
+    </div>
+   </div>
    <!-- Footer Section Ends -->
 
   </body>
