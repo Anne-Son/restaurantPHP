7cdfd3f08d1e15f9a5a3ba106e596dd3ebb9bd30
AnneSon <slhyuny@gmail.com>
2021-09-11 22:46:12 -0700

    partials

---
+++ admin/partials/footer.php
@@ -0,0 +1,13 @@
+  <!-- Footer Section Starts -->
+   <div class="footer">
+    <div class="wrapper">
+        <p class="text-center">
+          &copy 2021. All rights reserved. Galbi Restaurant. Designed by
+          <a href="www.anneson.com"> Anne Son </a>
+        </p>
+    </div>
+   </div>
+   <!-- Footer Section Ends -->
+
+  </body>
+</html>
