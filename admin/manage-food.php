f657bd7aff5ca259a84fe910779ac13ab82dd1f2
AnneSon <slhyuny@gmail.com>
2021-10-14 16:42:15 -0700

    manage food

--- admin/manage-food.php
+++ admin/manage-food.php
@@ -71,7 +71,7 @@
                   <td><?= $active; ?></td>
                   <td>
                      <a href="#" class="btn-secondary">Update Food</a>  
-                     <a href="#" class="btn-danger">Delete Food</a> 
+                     <a href="<?php echo SITEURL; ?>admin/delete-food.php?id=<?php echo $id; ?>&image_name=<?php echo $image_name; ?>" class="btn-danger">Delete Food</a> 
                   </td>
                </tr>
 
