68d911f214d002ab22872dd8b40105787b490cd7
anne-son <annesontech@gmail.com>
2021-10-15 12:48:27 -0700

    delete food

--- admin/delete-food.php
+++ admin/delete-food.php
@@ -1,4 +1,15 @@
 <?php
+includ('../config/constants.php');
 
+if(isset($_GET['id']) && isset($_GET['image_name']))
+{
+    //Process to Delete
+}
+else
+{
+    //Redirect to manage food page
+    $_SESSION['delete'] = "<div class='error'>Unauthorized Access</div>"
+    header('location:'.SITEURL.'admin/manage-food.php');
+}
 
 ?>
